6c364bcb84ea109947c849ada52d87f8dde0ee63
JonnySB <jonny-uk@hotmail.com>
2023-06-19 22:29:42 +0100

    updated user registration form

--- recipe_catalogue/users/views.py
+++ recipe_catalogue/users/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 from django.contrib import messages
 
 def login_user(request):
@@ -25,6 +28,32 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("Logout successful"))
     return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, (f"Registration successful - welcome {username}!"))
+            return redirect('home')
+
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'authenticate/register_user.html', {'form':form})
+
+class RegisterUserView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login_user')
+    template_name = 'authenticate/register_user.html'
+
+
+
    
 
 
